83a2070f7f37d9fcc893bae975c365eb2fc0b156
Reilbat <alexandre.2555@hotmail.fr>
2019-11-25 11:49:24 +0100

    1

---
+++ src/server.ts
@@ -0,0 +1,59 @@
+import express = require('express')
+import { MetricsHandler } from './metrics'
+import path = require('path')
+import bodyparser = require('body-parser')
+const dbMet: MetricsHandler = new MetricsHandler('./db/metrics')
+
+const app = express()
+const port: string = process.env.PORT || '8082'
+app.use(express.static(path.join(__dirname, '/../public')))
+
+app.set('views', __dirname + "/../views")
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req: any, res: any) => {
+  res.write('Hello world')
+  res.end()
+})
+
+app.get('/hello/:name', (req: any, res: any) => {
+  res.render('hello.ejs', {name: req.params.name})
+})
+
+// app.get('/metrics.json', (req: any, res: any) => {
+//    MetricsHandler.get((err: Error | null, result?: any) => {
+//      if (err) {
+//        throw err
+//     }
+//    res.json(result)
+//   })
+//  })
+
+app.post('/metrics/:id', (req: any, res: any) => {
+
+    dbMet.save(req.params.id, req.body, (err: Error | null) => {
+       if (err) throw err
+       res.status(200).send('ok')
+     })
+   })
+
+app.get('/metrics/:id', (req: any, res: any) => {
+    dbMet.get(req.params.id, (err: Error | null) => {
+     if (err) throw err
+     res.status(200).send('ok')
+  })
+ })
+
+  app.use(bodyparser.json()) 
+  app.use(bodyparser.urlencoded())
+
+
+app.listen(port, (err: Error) => {
+  if (err) {
+    throw err
+  }
+  console.log(`Server is running on http://localhost:${port}`)
+})
+
+
